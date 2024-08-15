5629ebc0f42b3ac4d550c8f2419991630d00cbb1
CadenceReynolds <reynocal001@warren.k12.in.us>
2024-08-15 13:28:21 -0400

    Finished Assignment

--- src/megaBytesConverter.java
+++ src/megaBytesConverter.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class megaBytesConverter {
+public class megaBytesConverter {
+    public static void main(String[] args) {
+        printMegaBytesAndKiloBytes(2500);
+        printMegaBytesAndKiloBytes(-1024);
+        printMegaBytesAndKiloBytes(5000);
+    }
+
+    public static void printMegaBytesAndKiloBytes(int kilobytes){
+        if(kilobytes < 0){
+            System.out.println("Negative KB cannot exist");
+        } else{
+            int mega = kilobytes / 1024;
+            int kb = kilobytes % 1024;
+            String result = String.format("%s KB = %s MB and %s KB", kilobytes, mega, kb);
+            System.out.println(result);
+        }
+    }
+
 }
